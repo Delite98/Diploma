bad41def6445bdace12d34cf21032d140704d84e
Darya <dashkash22@mail.ru>
2022-07-07 17:41:27 +0300

    API tests

---
+++ src/test/java/tests/api/ApiTests.java
@@ -0,0 +1,81 @@
+package tests.api;
+
+import baseEntities.BaseApiTest;
+import com.google.gson.Gson;
+import configurations.ApiEndpoints;
+import io.restassured.response.Response;
+import models.Project;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiTests extends BaseApiTest {
+
+
+    @Test
+    public void getProjectsApiTest() {
+        given()
+                .when()
+                .get(ApiEndpoints.GET_PROJECTS)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void getReportsApiTest() {
+        given()
+                .when()
+                .pathParam("project_id", 12)
+                .get(ApiEndpoints.GET_REPORTS)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+        @Test
+        public void getPlansApiTest() {
+        given()
+                .when()
+                .pathParam("project_id", 12)
+                .get(ApiEndpoints.GET_PLANS)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void postProjectApiTest(){
+        Project newProject = Project.builder()
+                .name("TestApi3+1")
+                .announcement("Hi, I'm test api for diploma")
+                .build();
+
+        given()
+                .body(String.format("{\n" +
+                        "  \"name\": \"%s\"\n" +
+                        "}", newProject.getName()))
+                .when()
+                .post(ApiEndpoints.ADD_PROJECT)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test (expectedExceptions = {AssertionError.class})
+    public void incorrectValidateApiTest() {
+        Response response = given()
+                .pathParam("project_id", 12)
+                .get(ApiEndpoints.GET_PROJECT);
+
+        Project actualProject = new Gson().fromJson(response.getBody().asString(),
+                Project.class);
+
+        Assert.assertEquals(actualProject.getName(), "Name of Project for API test");
+    }
+}
