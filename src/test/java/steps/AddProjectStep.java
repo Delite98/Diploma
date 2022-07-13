72037c9c4b90464b8c0b91c11dd98bfd68c1e146
bluekotte <Kurilo.Val@yandex.by>
2022-07-13 20:24:40 +0300

    code fix for project type radiobutton

--- src/test/java/steps/AddProjectStep.java
+++ src/test/java/steps/AddProjectStep.java
@@ -22,10 +22,13 @@ public class AddProjectStep extends BaseStep {
         switch (suiteMode) {
             case ("suite_mode_single"):
                 addProjectPage.getSingleSuiteModeRadioButtonLocator().click();
+                break;
             case ("suite_mode_single_baseline"):
                 addProjectPage.getBaselineSuiteModeRadioButtonLocator().click();
+                break;
             case ("suite_mode_multi"):
                 addProjectPage.getMultiSuiteModeRadioButtonLocator().click();
+                break;
         }
         if (projectCompletedCheckBoxButton) {
             addProjectPage.getProjectCompletedCheckBoxButtonLocator().click();
